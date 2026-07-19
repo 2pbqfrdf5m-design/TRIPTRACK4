caf46ef7cc9497e72c69d17bff137500b8736950
2pbqfrdf5m-design <rproulx89@gmail.com>
2026-07-19 07:53:14 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-const CACHE = 'cn-rail-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'trip-track-v2';
+const ASSETS = [
+  './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -14,7 +16,14 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(response => {
+      if (response.ok && new URL(e.request.url).origin === self.location.origin) {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+      }
+      return response;
+    }).catch(() => e.request.mode === 'navigate' ? caches.match('./index.html') : cached))
   );
 });
